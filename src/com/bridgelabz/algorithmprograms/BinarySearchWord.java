b57c50e703336fd363d8e2b1e94f256fb59cb6ad
varshini <dreamvarshini@gmail.com>
2021-09-02 14:22:37 +0530

    ADDED:binary search function to search for given word

---
+++ src/com/bridgelabz/algorithmprograms/BinarySearchWord.java
@@ -0,0 +1,59 @@
+package com.bridgelabz.algorithmprograms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+
+public class BinarySearchWord 
+{
+
+
+	public static void binarySearch(String listOfWords[],int firstIndex,int lastIndex,String key )
+	{
+		int middleIndex = (firstIndex + lastIndex)/2; 
+
+		while( firstIndex <= lastIndex )
+		{  
+			if ( listOfWords[middleIndex].compareTo(key)<0 )
+			{  
+				firstIndex = middleIndex + 1;     
+			}
+			else if ( listOfWords[middleIndex].compareTo(key)==0)
+			{  
+				System.out.println("given word : "+key+" is found at index: " + middleIndex);  
+				break;  
+			}
+			else
+			{  
+				lastIndex = middleIndex - 1;  
+			}  
+			middleIndex = (firstIndex + lastIndex)/2;  
+		}  
+		if ( firstIndex > lastIndex )
+		{  
+			System.out.println("given word : "+key+" is not found");  
+		} 
+
+
+	}
+
+	public static void main(String[] args) 
+
+	{
+		System.out.println("enter the list of words :");
+		Scanner scanner = new  Scanner(System.in);
+		String string=scanner.nextLine();
+
+		System.out.println("enter the key word to search");
+		String key=scanner.next();
+		String listOfWords[]=string.toLowerCase().split(",");
+
+		Arrays.sort(listOfWords);
+
+		binarySearch(listOfWords, 0, (listOfWords.length)-1, key);
+
+
+	}
+
+}
